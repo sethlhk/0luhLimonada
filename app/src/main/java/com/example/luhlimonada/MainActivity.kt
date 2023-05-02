0bc4126a500f06d9f99f9adb5714f647968d90c4
sethlhk <lauramodesto465@gmail.com>
2023-05-02 17:08:54 -0300

    Initial commit

---
+++ app/src/main/java/com/example/luhlimonada/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.luhlimonada
+
+import android.graphics.fonts.FontStyle
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.node.modifierElementOf
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.luhlimonada.ui.theme.LuhLimonadaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LuhLimonadaTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                           limoeiro()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun limoeiro(){
+
+    Column  (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+            ){
+        Text(
+            text = stringResource(R.string.limoeiro)
+                     , fontSize =  30.sp
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.limoeiro) ,
+            contentDescription = null,
+            modifier = Modifier
+                .size(350.dp)
+                .border(
+                    BorderStroke(4.dp, Color.Cyan),
+                    RoundedCornerShape(10.dp)
+                )
+
+
+        )
+
+
+    }
+
+}
